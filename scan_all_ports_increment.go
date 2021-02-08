e4b25e1000e7b77859f98816514a15b569ab6cfa
Gustavo Roberto Rodrigues Gonçalves <38466612+gustavorobertux@users.noreply.github.com>
2021-02-08 16:53:15 -0300

    Update scan_all_ports_increment.go

--- scan_all_ports_increment.go
+++ scan_all_ports_increment.go
@@ -16,13 +16,15 @@ func main() {
 	for PORT := 1; PORT < 65535; PORT++ {
 
 		TARGET := fmt.Sprintf("%s:%d", IP, PORT)
-
+		
 		// Change the value to 10 if you want to use it on your local network.
-
+		
 		_, err := net.DialTimeout("tcp", TARGET, 600*time.Millisecond)
 
 		if err != nil {
-		// comment the line below if you want to see only the open ports
+		
+			// comment the line below if you want to see only the open ports
+			
 			fmt.Printf("Port %d is closed\n", PORT)
 		} else {
 			fmt.Printf("Port %d is open\n", PORT)
